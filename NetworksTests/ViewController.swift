736a0de1bfce1d83de661021aff78bb0fc838252
Artem Belkov <bestK1ngArthur@aol.com>
2017-11-02 17:14:42 +0300

    Initial Commit

---
+++ NetworksTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetworksTests
+//
+//  Created by a.belkov on 02/11/2017.
+//  Copyright © 2017 bestK1ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
